8814efc2d767158e011af2f9da57d85c350e803c
Andrew Barker <103617658+Barkwanderer@users.noreply.github.com>
2023-11-19 22:04:45 -0500

    Set up chap 1 title page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import ItemsAndLives from './components/Items&Lives'
+import ChapterBox from './components/ChapterBox'
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
     <>
       <div className="App">
         White Witch
+        <ChapterBox />
         <ItemsAndLives />
       </div>  
     </>
--- src/components/ChapterBox.jsx
+++ src/components/ChapterBox.jsx
@@ -1,10 +1,15 @@
+import React from 'react';
+import '../assets/CSS/layout.css';
 
-
-
-{/* <div id="mainBox">
-<h2 id="headLine">Chapter One: The Siren In The Cove</h2>
-  <div class="cursorBox">
-    <h3 id="displayBlock">Press C to continue</h3>
-    <p id="bodyText"></p>
-  </div>
-</div> */}
+function ChapterBox() {
+    return (  
+      <div id="ChapOneTitlePage">
+        <h2 id="headLine">Chapter One: The Siren In The Cove</h2>
+        <div class="cursorBox">
+          <h3 id="displayBlock">Press C to continue</h3>
+          <p id="bodyText"></p>
+        </div>
+      </div>
+    );
+}
+export default ChapterBox;
