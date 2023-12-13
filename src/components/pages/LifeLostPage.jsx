e8d4a53fbc9bd294367217eefc9e4c77e2a4a0b9
Andrew Barker <103617658+Barkwanderer@users.noreply.github.com>
2023-12-12 23:47:40 -0500

    Replace SkullXBone image, restyle overlay boxes.

--- src/components/pages/LifeLostPage.jsx
+++ src/components/pages/LifeLostPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useEffect } from "react";
 
-import skullCrossBones from '../../assets/images/ui-elements/skull_crossbones.png';
+import skullCrossBones from '../../assets/images/ui-elements/SkullXBones.png';
 
 
 const LifeLostPage = ({ livesLeft, onClose }) => {
@@ -23,8 +23,8 @@ const LifeLostPage = ({ livesLeft, onClose }) => {
 
   return (
     <div id="lifeLostBox">
-      <img alt="skull and crossbones" src={skullCrossBones} width="100" height="100" />
       <p className="boldText">Oh no! You lost a life!</p>
+      <img alt="skull and crossbones" src={skullCrossBones} width="200" height="200" />
       <p>Don't worry, you can keep playing, but you only have {livesLeft} lives left.</p>
       <p className="boldText">Press Escape to continue.</p>
       <p>Good luck!</p>
