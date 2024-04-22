b50021d69742b23ddc5a5802231ed06912ff9085
Andrew Barker <103617658+Barkwanderer@users.noreply.github.com>
2024-04-22 09:30:54 -0400

    save game page simplefy text

--- src/components/pages/SaveGame.jsx
+++ src/components/pages/SaveGame.jsx
@@ -6,16 +6,19 @@ import Bandcamp from "../../assets/images/socials/bandcamp-icon.png";
 
 const SaveGame = ({ onSaveGame, onComplete }) => {
   return (
-    <div id="startBox">
+    <div id="startBox" className="widthControl">
       <div>
         <h3 id="startPrompt">
           Want to save your game?
         </h3>
-        <button onClick={onSaveGame} className="btn">Save Game</button>
       </div>
-      <div className="imageMaterialize imagePulse">
+      {/* <p className="standardText">Like the music you heard?</p> */}
+      <p className="standardText ">By registering, not only will you be able to store your progress, you will receive a free download code for the album 'White Witch'!</p>
+      {/* <div className="imageMaterialize imagePulse">
         <img className="borderRadius" alt="The bandcamp music distributionicon" src={Bandcamp} width="110" height="50"></img>
-      </div>
+      </div> */}
+      <button onClick={onSaveGame} className="btn">Save Game</button>
+
     </div>
   );
 }
