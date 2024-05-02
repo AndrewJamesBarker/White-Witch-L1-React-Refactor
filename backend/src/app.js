6cd80862faa02fd79366142708de2fd8c7f6adba
Andrew Barker <103617658+Barkwanderer@users.noreply.github.com>
2024-05-01 22:29:59 -0400

    Mongo atlas server running and connected with dummy data, compass also connected.

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,17 +19,14 @@ app.get('/', (req, res) => {
 // Setup database connection
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected successfully.');
   } catch (err) {
     console.error('MongoDB connection error:', err);
-    // Exit process with failure
     process.exit(1);
   }
 };
+
 
 connectDB();
 
