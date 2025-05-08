92f00642388977681ac23eeae44df1342e04725a
Andrew Barker <103617658+Barkwanderer@users.noreply.github.com>
2025-05-08 17:24:06 -0400

    tweak comments

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // Ensure this matches your Vite environment variable
+  baseURL: import.meta.env.VITE_API_URL, // Matches with Vite env variable
   withCredentials: true, // Send cookies with requests
 });
 
