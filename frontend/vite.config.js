313a2b473d1f82ba10135d4e467a6a3aa1ea1bd3
Andrew Barker <103617658+Barkwanderer@users.noreply.github.com>
2025-05-22 22:19:16 -0400

    meta description added, vite config add souremaps

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,17 @@
-import path from "path"
+import path from "path";
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  css: {
+    devSourcemap: true
+  },
+  build: {
+    sourcemap: true  
+  },
+  plugins: [react()], 
   server: {
-    port: process.env.PORT || 5173 // Uses the environment variable or defaults to 3000
+    port: process.env.PORT || 5173  
   },
   resolve: {
     alias: {
